95057f3b195b152544997c5ee781fc9dfcb38233
gtasa3box <gtasa3box@gmail.com>
2022-02-07 13:58:07 +0300

    com

--- src/js/global.js
+++ src/js/global.js
@@ -3,3 +3,37 @@ $('.reviews__btn-show').on('click', function(){
     $(this).toggleClass('active')  
     $($(this).data('target')).slideToggle()
 });
+
+//teacher slider
+$('.slider-teachers').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
